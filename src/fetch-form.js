c1526109f4736c0598fd069ceba9d2ff65e7ca58
Pranav Pise <itspranavpise@gmail.com>
2025-04-16 12:57:39 +0530

    doing everything manyally for now

--- src/fetch-form.js
+++ src/fetch-form.js
@@ -3,13 +3,13 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const companyId = "4266122";
+const projectId = "263763";
 const accessToken = process.env.PROCORE_ACCESS_TOKEN;
 const refreshToken = process.env.PROCORE_REFRESH_TOKEN;
 // Use the sandbox credentials since we're using the sandbox
 const clientId = process.env.PROCORE_SANBOX_CLIENT_ID;
 const clientSecret = process.env.PROCORE_SANDBOX_CLIENT_SECRET;
-const procore_base_url =
-  process.env.PROCORE_SANDBOX_BASE_URL;
+const procore_base_url = process.env.PROCORE_SANDBOX_BASE_URL;
 
 function refreshAccessToken() {
   return new Promise((resolve, reject) => {
@@ -75,5 +75,55 @@ function fetchChecklistData(token) {
   });
 }
 
+function fetchChecklistComments(token) {
+  const options = {
+    method: "GET",
+    url: `${procore_base_url}/rest/v1.0/projects/${projectId}/checklist/list_item_comments`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Procore-Company-Id": companyId,
+    },
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    if (response.statusCode === 401) {
+      console.log("Token expired, refreshing...");
+      refreshAccessToken()
+        .then((newToken) => fetchChecklistData(newToken))
+        .catch((err) => console.error("Error refreshing token:", err));
+    } else {
+      console.log(body);
+    }
+  });
+}
+
+function fetchRecycledChecklistComments(token) {
+  const options = {
+    method: "GET",
+    url: `${procore_base_url}/rest/v1.0/projects/${projectId}/recycle-bin/checklist/list_item_comments`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Procore-Company-Id": companyId,
+    },
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    if (response.statusCode === 401) {
+      console.log("Token expired, refreshing...");
+      refreshAccessToken()
+        .then((newToken) => fetchChecklistData(newToken))
+        .catch((err) => console.error("Error refreshing token:", err));
+    } else {
+      console.log(body);
+    }
+  });
+}
+
 console.log("Attempting to fetch data with existing token...");
 fetchChecklistData(accessToken);
+fetchChecklistComments(accessToken);
+fetchRecycledChecklistComments(accessToken);
