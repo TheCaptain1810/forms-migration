11ea5f56d10ad08ee08f9f3bb1c55be8ed46ed28
Pranav Pise <itspranavpise@gmail.com>
2025-04-16 15:31:38 +0530

    still working on it

--- src/fetch-form.js
+++ src/fetch-form.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const companyId = "4266122";
-const projectId = "263763";
+const projectId = "121313";
 const accessToken = process.env.PROCORE_ACCESS_TOKEN;
 const refreshToken = process.env.PROCORE_REFRESH_TOKEN;
 // Use the sandbox credentials since we're using the sandbox
@@ -123,7 +123,132 @@ function fetchRecycledChecklistComments(token) {
   });
 }
 
+function fetchDefaultDistributionMembers(token) {
+  const options = {
+    method: "GET",
+    url: `${procore_base_url}/rest/v1.0/checklist/default_distribution`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Procore-Company-Id": companyId,
+    },
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    if (response.statusCode === 401) {
+      console.log("Token expired, refreshing...");
+      refreshAccessToken()
+        .then((newToken) => fetchChecklistData(newToken))
+        .catch((err) => console.error("Error refreshing token:", err));
+    } else {
+      console.log(body);
+    }
+  });
+}
+
+function fetchChecklistItemAttachments(token) {
+  const options = {
+    method: "GET",
+    url: `${procore_base_url}/rest/v1.0/projects/${projectId}/checklist/list_item_attachments`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Procore-Company-Id": companyId,
+    },
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    if (response.statusCode === 401) {
+      console.log("Token expired, refreshing...");
+      refreshAccessToken()
+        .then((newToken) => fetchChecklistData(newToken))
+        .catch((err) => console.error("Error refreshing token:", err));
+    } else {
+      console.log(body);
+    }
+  });
+}
+
+function fetchChecklistItemObservations(token) {
+  const options = {
+    method: "GET",
+    url: `${procore_base_url}/rest/v1.0/projects/${projectId}/checklist/list_item_observations`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Procore-Company-Id": companyId,
+    },
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    if (response.statusCode === 401) {
+      console.log("Token expired, refreshing...");
+      refreshAccessToken()
+        .then((newToken) => fetchChecklistData(newToken))
+        .catch((err) => console.error("Error refreshing token:", err));
+    } else {
+      console.log(body);
+    }
+  });
+}
+
+function fetchChecklistItemTypes(token) {
+  const options = {
+    method: "GET",
+    url: `${procore_base_url}/rest/v1.0/checklist/item_types`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Procore-Company-Id": companyId,
+    },
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    if (response.statusCode === 401) {
+      console.log("Token expired, refreshing...");
+      refreshAccessToken()
+        .then((newToken) => fetchChecklistData(newToken))
+        .catch((err) => console.error("Error refreshing token:", err));
+    } else {
+      console.log(body);
+    }
+  });
+}
+
+function fetchChecklistItems(token) {
+  const options = {
+    method: "GET",
+    url: `${procore_base_url}/rest/v1.0/projects/${projectId}/checklist/list_items`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Procore-Company-Id": companyId,
+    },
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    if (response.statusCode === 401) {
+      console.log("Token expired, refreshing...");
+      refreshAccessToken()
+        .then((newToken) => fetchChecklistData(newToken))
+        .catch((err) => console.error("Error refreshing token:", err));
+    } else {
+      console.log(body);
+    }
+  });
+}
+
 console.log("Attempting to fetch data with existing token...");
 fetchChecklistData(accessToken);
 fetchChecklistComments(accessToken);
 fetchRecycledChecklistComments(accessToken);
+fetchDefaultDistributionMembers(accessToken);
+fetchChecklistItemAttachments(accessToken);
+fetchChecklistItemObservations(accessToken);
+fetchChecklistItemTypes(accessToken);
+fetchChecklistItems(accessToken);
